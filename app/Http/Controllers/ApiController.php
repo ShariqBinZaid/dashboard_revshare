3a3f080394264a0ee7bb259d383be181259ac8d1
Muhammad Shariq Bin Zaid <shariqbinzaid@gmail.com>
2023-09-27 17:40:53 +0500

    1

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -26,7 +26,6 @@ class ApiController extends Controller
             'dob' => 'required',
             'phone' => 'required',
             'status' => 'required',
-            // 'status' => 'required',
             'user_type' => 'required',
         ]);
 
